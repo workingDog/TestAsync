72b36dd513547d556f3867864a4bcdb90636a0c8
smithdude <ringowathelet@gmail.com>
2021-07-12 11:33:25 +0900

    Initial Commit

---
+++ TestAsync/TestAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAsyncApp.swift
+//  TestAsync
+//
+//  Created by Ringo Wathelet on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct TestAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
